607d447483a6fb693fb7a511b641b174426e3f13
hilla-m <hilla.manbar@gmail.com>
2021-04-14 16:24:48 +0300

    movie-app

--- src/pages/ActorsPage/ActorsPage.js
+++ src/pages/ActorsPage/ActorsPage.js
@@ -17,8 +17,12 @@ function ActorsPage(props) {
         new Actor("Emma", "Stone", "November 6, 1988", "https://m.media-amazon.com/images/M/MV5BMjI4NjM1NDkyN15BMl5BanBnXkFtZTgwODgyNTY1MjE@._V1_UX214_CR0,0,214,317_AL_.jpg", "https://www.imdb.com/name/nm1297015/"),
         new Actor("Ryan", "Gosling", "November 12, 1980", "https://m.media-amazon.com/images/M/MV5BMTQzMjkwNTQ2OF5BMl5BanBnXkFtZTgwNTQ4MTQ4MTE@._V1_UY317_CR18,0,214,317_AL_.jpg", "https://www.imdb.com/name/nm0331516/"),
         new Actor("Charlize", "Theron", "August 7, 1975", "https://m.media-amazon.com/images/M/MV5BMTk5Mzc4ODU0Ml5BMl5BanBnXkFtZTcwNjU1NTI0Mw@@._V1_UY317_CR12,0,214,317_AL_.jpg", "https://www.imdb.com/name/nm0000234/"),
-        new Actor("Bradley", "Cooper", "January 5, 1975", "https://m.media-amazon.com/images/M/MV5BMjM0OTIyMzY1M15BMl5BanBnXkFtZTgwMTg0OTE0NzE@._V1_UX214_CR0,0,214,317_AL_.jpg", "https://www.imdb.com/name/nm0177896/")
+        new Actor("Bradley", "Cooper", "January 5, 1975", "https://m.media-amazon.com/images/M/MV5BMjM0OTIyMzY1M15BMl5BanBnXkFtZTgwMTg0OTE0NzE@._V1_UX214_CR0,0,214,317_AL_.jpg", "https://www.imdb.com/name/nm0177896/"),
+        new Actor("Keira", "Knightley", "March 26, 1985", "https://m.media-amazon.com/images/M/MV5BMTYwNDM0NDA3M15BMl5BanBnXkFtZTcwNTkzMjQ3OA@@._V1_UY317_CR6,0,214,317_AL_.jpg", "https://www.imdb.com/name/nm0461136/"),
+        new Actor("Matthew", "McConaughey", "November 4, 1969", "https://m.media-amazon.com/images/M/MV5BMTg0MDc3ODUwOV5BMl5BanBnXkFtZTcwMTk2NjY4Nw@@._V1_UX214_CR0,0,214,317_AL_.jpg", "https://www.imdb.com/name/nm0000190/")
+    
     ]);
+
 
     //src/assets/images/BradleyCooper.jpg   // ../../assets/images/BradleyCooper.jpg   //path not working
 
@@ -92,7 +96,7 @@ return (
             <Form.Row class="form-row">
                 <Form.Label className="label">Search</Form.Label>
                 {/* <Form.Control type="text" className="filter" placeholder="Search..." value={filterText} onChange={e => setFilterText(e.target.value)} /> */}
-                <Form.Control type="text" className="filter" placeholder="Search..." value={filterText} onChange={e => checkFilter(e.target.value)} />
+                <Form.Control type="text" className="filter" placeholder="First name / Last Name" value={filterText} onChange={e => checkFilter(e.target.value)} />
                 <Form.Label className="label">Sort By</Form.Label>
                 {/* <Form.Control as="select" className="sort" value={sortBy} onChange={e => setSortBy(e.target.value)}> */}
                 <Form.Control as="select" className="sort" value={sortBy} onChange={e => checkSortBy(e.target.value)}>
@@ -102,7 +106,7 @@ return (
                 </Form.Control>
             </Form.Row>
         </Form>
-        <Row className="cards-div" xs={1} md={2} lg={4}>
+        <Row className="cards-div" xs={1} md={2} lg={5}>
             {actorsCards}
         </Row>
     </Container>
