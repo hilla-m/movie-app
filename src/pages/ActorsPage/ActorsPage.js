be0823a8611e5cd548bad6047a13c5175168763f
hilla-m <hilla.manbar@gmail.com>
2021-04-19 18:03:12 +0300

    movie-app fixes

--- src/pages/ActorsPage/ActorsPage.js
+++ src/pages/ActorsPage/ActorsPage.js
@@ -3,13 +3,13 @@ import Actor from "../../model/ActorModel/ActorModel";
 import { Container, Row, Col, Form } from 'react-bootstrap';
 import ActorCard from "../../components/ActorCard/ActorCard";
 import './ActorsPage.css';
-// import "../../assets/images/BradleyCooper.jpg";  //not working
+//import '../../assets/images/BradleyCooper.jpg'; //not working
 
 function ActorsPage(props) {
 
     const [filterText, setFilterText] = useState("");
 
-    const [sortBy, setSortBy] = useState("");
+    const [sortBy, setSortBy] = useState("firstName");
 
     const [actors, setActors] = useState([
         new Actor("Johnny", "Depp", "June 9, 1963", "https://m.media-amazon.com/images/M/MV5BMTM0ODU5Nzk2OV5BMl5BanBnXkFtZTcwMzI2ODgyNQ@@._V1_UY317_CR4,0,214,317_AL_.jpg", "https://www.imdb.com/name/nm0000136/"),
@@ -20,97 +20,48 @@ function ActorsPage(props) {
         new Actor("Bradley", "Cooper", "January 5, 1975", "https://m.media-amazon.com/images/M/MV5BMjM0OTIyMzY1M15BMl5BanBnXkFtZTgwMTg0OTE0NzE@._V1_UX214_CR0,0,214,317_AL_.jpg", "https://www.imdb.com/name/nm0177896/"),
         new Actor("Keira", "Knightley", "March 26, 1985", "https://m.media-amazon.com/images/M/MV5BMTYwNDM0NDA3M15BMl5BanBnXkFtZTcwNTkzMjQ3OA@@._V1_UY317_CR6,0,214,317_AL_.jpg", "https://www.imdb.com/name/nm0461136/"),
         new Actor("Matthew", "McConaughey", "November 4, 1969", "https://m.media-amazon.com/images/M/MV5BMTg0MDc3ODUwOV5BMl5BanBnXkFtZTcwMTk2NjY4Nw@@._V1_UX214_CR0,0,214,317_AL_.jpg", "https://www.imdb.com/name/nm0000190/")
-    
     ]);
+    // ../../assets/images/BradleyCooper.jpg   //path not working
 
+    // filtering actors
+    const filteredActors = actors.filter(actor => actor.firstName.toLowerCase().includes(filterText.toLowerCase()) || actor.lastName.toLowerCase().includes(filterText.toLowerCase()))
 
-    //src/assets/images/BradleyCooper.jpg   // ../../assets/images/BradleyCooper.jpg   //path not working
-
-    let actorsCards = actors.map(actor => <ActorCard actor={actor} />);    //className={checkFilter(actor) ? "show" : "hide"} 
-
-    // function checkFilter(actor) {
-    //     if (filterText === "" || actor.firstName.toLowerCase().includes(filterText.toLowerCase()) || actor.lastName.toLowerCase().includes(filterText.toLowerCase())) {
-    //         return true;
-    //     }
-    //     return false;
-    // }
-
-
-
-function checkFilter(value) {
-    debugger;
-    setFilterText(value);
-
-        let filteredActors = actors.filter(actor => actor.firstName.toLowerCase().includes(value.toLowerCase()) || actor.lastName.toLowerCase().includes(value.toLowerCase()))
-
-        setActors(filteredActors);
-        actorsCards = actors.map(actor => <ActorCard actor={actor} />);     
-}
-
-
-function checkSortBy(value) {
-    setSortBy(value);
-    let sortedActors = [];
-
-    if (value === "firstName") {
-        sortedActors = actors.sort(function (a, b) {
-            if (a.firstName.toUpperCase() < b.firstName.toUpperCase()) {
-                return -1;
-            }
-            if (a.firstName.toUpperCase() > b.firstName.toUpperCase()) {
-                return 1;
-            }
+    // sorting actors
+    filteredActors.sort((actor1, actor2) => {
+        if (actor1[sortBy] > actor2[sortBy]) {
+            return 1;
+        } else if (actor1[sortBy] < actor2[sortBy]) {
+            return -1;
+        } else {
             return 0;
-        });
-    }
+        }
+    });
 
-    if (value === "lastName") {
-        sortedActors = actors.sort(function (a, b) {
-            if (a.lastName.toUpperCase() < b.lastName.toUpperCase()) {
-                return -1;
-            }
-            if (a.lastName.toUpperCase() > b.lastName.toUpperCase()) {
-                return 1;
-            }
-            return 0;
-        });
-    }
+    // actors for display 
+    const actorsCards = filteredActors.map(actor => <ActorCard actor={actor} />);
 
-    if (value === "age") {
-        sortedActors = actors.sort(function (a, b) {
-            return a.age - b.age;
-        });
-    }
-
-    setActors(sortedActors);
-    actorsCards = actors.map(actor => <ActorCard actor={actor} />);
-}
-
-
-return (
-    <Container className="container">
-        <Row className="title">
-            <h1>Actors Gallery</h1>
-        </Row>
-        <Form>
-            <Form.Row class="form-row">
-                <Form.Label className="label">Search</Form.Label>
-                {/* <Form.Control type="text" className="filter" placeholder="Search..." value={filterText} onChange={e => setFilterText(e.target.value)} /> */}
-                <Form.Control type="text" className="filter" placeholder="First name / Last Name" value={filterText} onChange={e => checkFilter(e.target.value)} />
-                <Form.Label className="label">Sort By</Form.Label>
-                {/* <Form.Control as="select" className="sort" value={sortBy} onChange={e => setSortBy(e.target.value)}> */}
-                <Form.Control as="select" className="sort" value={sortBy} onChange={e => checkSortBy(e.target.value)}>
-                    <option value="firstName">First Name</option>
-                    <option value="lastName">Last Name</option>
-                    <option value="age">Age</option>
-                </Form.Control>
-            </Form.Row>
-        </Form>
-        <Row className="cards-div" xs={1} md={2} lg={5}>
-            {actorsCards}
-        </Row>
-    </Container>
-);
+    return (
+        <Container className="container">
+            <Row className="title">
+                <h1>Actors Gallery</h1>
+            </Row>
+            <Form>
+                <Form.Row class="form-row">
+                    <Form.Label className="label">Search</Form.Label>
+                    <Form.Control type="text" className="filter" placeholder="Search..." value={filterText} onChange={e => setFilterText(e.target.value)} />
+                    <Form.Label className="label">Sort By</Form.Label>
+                    <Form.Control as="select" className="sort" value={sortBy} onChange={e => setSortBy(e.target.value)}>
+                        <option value="firstName">First Name</option>
+                        <option value="lastName">Last Name</option>
+                        <option value="age">Age</option>
+                    </Form.Control>
+                </Form.Row>
+            </Form>
+            <Row className="cards-div" xs={1} md={2} lg={5}>
+                {actorsCards}
+            </Row>
+        </Container>
+    );
 }
 
 export default ActorsPage;
